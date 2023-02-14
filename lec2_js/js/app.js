562db26be4fe5fb8a4b60b35fd054bbe2c78ef5f
yahyaelsf <yahyaelsaftawi98@gmail.com>
2023-02-14 22:16:34 +0300

    edit

---
+++ lec2_js/js/app.js
@@ -0,0 +1,92 @@
+// var a = 'aaa';
+// var a = 'bbb';
+// let x =10
+// x=20;
+// const pi = 3.14;
+// console.log(a,x)
+// conditions 
+// if()
+// if()else{}
+// else if()
+// switch
+
+// let age = 10 ;
+// if(age > 0){
+//     if (age >= 18 && age < 60) {
+//         console.log('you are youth');
+//     } else if (age > 60) {
+//         console.log('you are old');
+//     }
+//     else {
+//         console.log('you are baby');
+//     }
+// }
+// let num = 1;
+// switch(num){
+//     case 1:
+//     case 2:  
+//     console.log('this is two');
+//     break;
+//     default : 
+//         console.log('this is default');
+// }
+
+let arr = [20,30,40,50,20,30];
+// arr.push(60);
+// arr.pop();
+// arr.shift();
+// arr.unshift(70);
+// console.log(arr);
+// let uniq = new Set(arr);
+// console.log(uniq)
+//
+// 1-for 
+// 2- while 
+// 3- do while 
+
+// for(let i =0 ; i <arr.length ; i++){
+//     console.log(arr[i])
+// }
+// let i = 0 ;
+// while(i<arr.length){
+//     console.log(arr[i]);
+//     i++;
+// }
+// let i =0 ;
+// do{
+//     console.log(arr[i]);
+//     i++;
+// }while(i<arr.length)
+// let p = "<div>"
+// p += "<p> Hollo</P>"
+// p +="</div>"
+// let name = 'yahy';
+// let p = `
+// <div>
+// <p> Hollo ${name}</P>
+// </div>
+// `
+// console.log(p)
+
+// $x = 10;//11 //12
+// $y = 5; //6
+// $x++ + $y++ - $x++ + ++$y * 2; 
+// 10 + 5 - 11 +7*2
+// 10 + 5 - 11 +14 => 18;
+
+// 18=> 3
+// 16 => 2
+// 13 => 1
+// 22 =>
+//  let text = "wellcome in gaza";
+//  text =text.slice(-5);
+//  console.log(text);
+// let text = "wellcome in gaza";
+// text = text.split(' ');
+// text = text.join(' ')
+//   console.log(text);
+
+let num = 8.6;
+// num = num.toFixed(2);
+
+console.log(Math.floor(num));
